e54eed17b88d844cd87b7bdbac68c2a2d6302fe8
Manjit Chakraborty <115330218+manjit32987@users.noreply.github.com>
2026-09-10 18:25:33 +0530

    m

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 const MAINTENANCE_CONFIG = {
   enabled: true, // MASTER SWITCH: set to false to open portal to all visitors
   devPasscode: "TIT_DEV_2026",
-  spocPasscode: "TIT_SIH_2026#SPOC",
+  spocPasscode: "TIT_SIH_0000#SPOC",
   title: "TIT SIH 2026 • Upgrades in Progress",
   heading: "System Upgrades in Progress",
   subheading: "Institution Innovation Council (IIC) • Tripura Institute of Technology",
